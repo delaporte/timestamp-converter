5d2fbae16ecee02898401f49bf7e7554fa886e04
ade <alexis.delaporte@amadeus.fr>
2015-02-04 16:29:07 +0100

    initial revision

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name timestampConverterApp
+ * @description
+ * # timestampConverterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('timestampConverterApp', ['ui.bootstrap']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name timestampConverterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the timestampConverterApp
+ */
+angular.module('timestampConverterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
